2eb864790510a7070c4174097fd6ae8da4108539
louellrmnn <ellermannlouise@gmail.com>
2024-10-02 16:05:56 +0200

    exo4

---
+++ exo4.py
@@ -0,0 +1,19 @@
+#4a
+nombres = [1, 2, 3, 4, 5]
+nombres = nombres + [6]
+print(nombres)
+
+
+#4b
+monDictionnaire = {
+    "nom": "Louise",
+    "age": 24,
+    "ville": "Montpellier"
+}
+cle = "age"
+print(monDictionnaire[cle])
+
+
+
+
+
