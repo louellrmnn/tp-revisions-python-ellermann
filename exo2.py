fb0d8d075ef604ed8465a91a4d49c8094531a000
louellrmnn <ellermannlouise@gmail.com>
2024-10-02 14:45:13 +0200

    exo2

---
+++ exo2.py
@@ -0,0 +1,11 @@
+a = input("Un nombre:")
+converted_a = int(a)
+
+if converted_a < 0:
+    print("nombre negatif")
+
+elif converted_a == 0: 
+    print("nombre nul")
+
+else: 
+    print("nombre positif")
